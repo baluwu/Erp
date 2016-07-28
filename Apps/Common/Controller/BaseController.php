321aa28a16c6dd42249afa68de3f9da47a2c5748
root <root@iZ946axmniaZ.(none)>
2016-07-28 17:18:55 +0800

    -

--- Apps/Common/Controller/BaseController.php
+++ Apps/Common/Controller/BaseController.php
@@ -8,7 +8,7 @@ class BaseController extends \Phalcon\Mvc\Controller {
      * 返回JSON格式信息
      *
      * @param String $data 数据正文
-     * @param String $code 状态码
+     * @param integer $code 状态码
      * @param String $msg 消息体
      * @return void
      */
@@ -16,6 +16,7 @@ class BaseController extends \Phalcon\Mvc\Controller {
         ob_clean();
 
         header('Content-type: application/json');
+        http_response_code($code);
 
         echo json_encode([
             'succ' => $code === 200,
@@ -31,16 +32,19 @@ class BaseController extends \Phalcon\Mvc\Controller {
      * 返回JSONP格式信息
      *
      * @param String $data 数据正文
-     * @param String $code 状态码
+     * @param integer $code 状态码
      * @param String $msg 消息体
      * @return void
      */
     public function echoJsonp($data, $code = 200, $msg = '') {
-        if (!isset($_GET['callback'])) return ;
+        if (!isset($_GET['callback'])) { 
+            return $this->echoJson($data, $code, $msg);
+        }
 
         ob_clean();
 
         header('Content-type: application/json');
+        http_response_code($code);
 
         echo trim($_GET['callback']) . '(' . json_encode([
             'succ' => $code === 200,
